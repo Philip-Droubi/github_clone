88fe3196277178f6de413142a1fa2f8417d39d22
Philip <philipdroubi@gmail.com>
2023-11-12 04:01:08 +0200

    Store Group

--- app/Http/Controllers/Groups/GroupController.php
+++ app/Http/Controllers/Groups/GroupController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Groups;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GroupRequest;
+use App\Models\Group\GroupUser;
 use Illuminate\Http\Request;
 use App\Models\Group\Group;
 use App\Traits\GeneralTrait;
 use App\Traits\HelperTrait;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
@@ -23,13 +25,22 @@ class GroupController extends Controller
     public function store(GroupRequest $request)
     {
         DB::beginTransaction();
-        Group::create([
+        $group = Group::create([
             "name" => $request->name,
             "description" => $request->description,
             "group_key" => $this->generateUniqeStringKey(Group::class, 'group_key', Config::get('custom.group_key_length', 32)),
             "is_public" => false,
             "created_by" => auth()->user()->id,
         ]);
+        GroupUser::create([
+            "group_id" => $group->id,
+            "user_id" => auth()->id(),
+        ]);
+        foreach ($request->users_list as $id)
+            GroupUser::firstOrCreate([
+                "group_id" => $group->id,
+                "user_id" => $id,
+            ]);
         DB::commit();
         return $this->success();
     }
